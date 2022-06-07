fa37157d29a5445414418940a1b3fd25709505be
AiFx4Intel <89227642+AiFxApp@users.noreply.github.com>
2022-06-07 13:12:10 +0200

    simplify routes

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
 Route::get('/home', function() {
     return view('home');
 })->name('home')->middleware('auth');
