83a7abfa257a5fb99f0c3085f4bda03f83c958dc
AiFx4Intel <89227642+AiFxApp@users.noreply.github.com>
2022-06-07 17:03:12 +0200

    profile update - input fields

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -87,25 +87,28 @@
       <hr class="mb-3">
 
     <div class="form-group row">
-      <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputName2" placeholder="Name">
-      </div>
-    </div>
-    <div class="form-group row">
       <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
-      <div class="col-sm-10">
+      <div class="col-sm-6">
         <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
       </div>
     </div>
+
     <div class="form-group row">
       <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-      <div class="col-sm-10">
+      <div class="col-sm-6">
         <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
       </div>
     </div>
+
     <div class="form-group row">
-      <div class="offset-sm-2 col-sm-10">
+        <label for="inputExperience" class="col-sm-2 col-form-label">Notes</label>
+        <div class="col-sm-6">
+          <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+        </div>
+    </div>
+
+    <div class="form-group row">
+      <div class="offset-sm-2 col-sm-6">
         <div class="checkbox">
           <label>
             <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
